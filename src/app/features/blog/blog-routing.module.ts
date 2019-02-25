896312939e4aca941c5745801e9ac4b82fe46c1c
cassanellicarlo <cassatek@gmail.com>
2019-02-25 12:41:13 +0100

    added new post guard and jumbo img

--- src/app/features/blog/blog-routing.module.ts
+++ src/app/features/blog/blog-routing.module.ts
@@ -3,10 +3,11 @@ import { Routes, RouterModule } from '@angular/router';
 import { PostListComponent } from './post-list/post-list.component';
 import { NewPostComponent } from './new-post/new-post.component';
 import { PostDetailComponent } from './post-detail/post-detail.component';
+import { AuthGuardService } from 'src/app/core/guards/auth-guard.service';
 
 const routes: Routes = [
   { path: '' , component: PostListComponent},
-  { path: 'new' , component: NewPostComponent},
+  { path: 'new' , component: NewPostComponent, canActivate: [AuthGuardService]},
   { path: 'post/:id' , component: PostDetailComponent}
 ];
 
